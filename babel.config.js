bf7b503190099b2aa4987c7687f0333c0fa88460
Drew Bartlett <drewb9@gmail.com>
2023-02-17 19:06:27 -0700

    back to microbundle

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = () => {
-  const presets = [['@babel/preset-env', { modules: false }]];
-  const plugins = ['@babel/plugin-transform-runtime'];
-
-  return {
-    plugins,
-    presets,
-  };
-};
